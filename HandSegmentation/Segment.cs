7c1c0db49182e185c800e9c4ae901c692a822bc9
SankethBennur <snkthbnnr3@gmail.com>
2021-01-16 23:24:59 +0000

    Add files via upload

---
+++ HandSegmentation/Segment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using AForge.Imaging.Filters;
+using System.Drawing;
+
+namespace HandSegmentation
+{
+    public class Segment
+    {
+        private bool isSkin(Color c)
+        {
+            return (c.R > 95 && c.G > 40 && c.B > 20) &&
+                (Math.Max(c.R, Math.Max(c.G, c.R)) -
+                Math.Min(c.R, Math.Min(c.G, c.B))) > 15 &&
+                            (Math.Abs(c.R - c.G) > 15 && c.R > c.G && c.R > c.G);
+        }
+
+    }
+}
